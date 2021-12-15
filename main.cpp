a258b0234e49730cea04d3ce45c06d2747bf5c74
jortega99 <johnortega99@gmail.com>
2021-12-15 12:54:21 -0800

    added if-else to check if map is empty

--- main.cpp
+++ main.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main(int argc, char** args){
     MostActiveCookie myobj;
     myobj.CreateMap(args[1],args[3]);
-    int value = myobj.FindMaxValue();
-    myobj.printMostActiveCookie(value);
+    map<string,int> Map;
+    Map = myobj.getMap();
+
+    if(!Map.empty()){
+        int value = myobj.FindMaxValue();
+        myobj.printMostActiveCookie(value);
+    }
+    else{
+        cout << "No cookies for selected date" << endl;
+    }
 
     return 0;
 }
